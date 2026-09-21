b13db85e85d68b6f692ffd2d4114283d29fc0d69
yuliiapikh <Yuliia.Pikh.RI.2026@lpnu.ua>
2026-09-21 18:36:01 +0300

    Перша зміна

--- Lab_02.1.cpp
+++ Lab_02.1.cpp
@@ -13,16 +13,16 @@ int main()
 {
     double Pi = 4 * atan(1.); // число пі
     double Alpha; // вхідний параметр
-    double z1; // результат обчислення 1-го виразу
+    //double z1; // результат обчислення 1-го виразу
     double z2; // результат обчислення 2-го виразу
 
     cout << "Alpha = "; cin >> Alpha;
 
-    z1 = cos(Alpha) + sin(Alpha) + cos(3 * Alpha) + sin(3 * Alpha);
+    //z1 = cos(Alpha) + sin(Alpha) + cos(3 * Alpha) + sin(3 * Alpha);
     z2 = 2 * sqrt(2) * cos(Alpha) * sin((Pi / 4) + 2 * Alpha);
 
     cout << endl;
-    cout << "z1 = " << z1 << endl;
+    //cout << "z1 = " << z1 << endl;
     cout << "z2 = " << z2 << endl;
 
     cin.get();
